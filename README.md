c13d096a483e76c8f90f391861ad53701de484f9
Alex Katopodis <akatopo@users.noreply.github.com>
2016-09-09 14:19:38 +0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tools: #⃣, 📚, 🎞, and 🖖
+> A [Bespoke.js](http://markdalgleish.com/projects/bespoke.js) presentation, built with [generator-bespoke](https://github.com/markdalgleish/generator-bespoke)
+
+Created for the September [DevStaff meet up](http://www.meetup.com/DevStaff-A-Developer-Community-Gathering-In-Crete) series of lightning talks.
+
+## View slides locally
+
+First, ensure you have the following installed:
+
+1. [Node.js](http://nodejs.org)
+2. [Bower](http://bower.io): `$ npm install -g bower`
+3. [Gulp](http://gulpjs.com): `$ npm install -g gulp-cli`
+
+Then, install dependencies and run the preview server:
+
+```bash
+$ npm install && bower install
+$ gulp serve
+```
---
+++ src/scripts/main.js
@@ -0,0 +1,34 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  theme = require('bespoke-theme-build-wars'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress');
+
+// Bespoke.js
+bespoke.from('article', [
+  theme(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
+require('domready')(function () {
+  require('twemoji').parse(document.body, {
+    folder: 'svg',
+    ext: '.svg'
+  });
+});
